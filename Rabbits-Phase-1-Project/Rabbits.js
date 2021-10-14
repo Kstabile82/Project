4adc96cb995c4682ff3421e99b592b7c18881d38
KarinaStabile <karina.stabile@gmail.com>
2021-10-14 01:07:35 -0400

    new commit, added returnMatches function

--- Rabbits-Phase-1-Project/Rabbits.js
+++ Rabbits-Phase-1-Project/Rabbits.js
@@ -1,16 +1,13 @@
 let inputObj = {
 }; 
-let counter = 0; 
-let rabbitMatches = []; 
+let matchArr = []; 
 let collection = document.getElementsByTagName('select');
 let domesticStr = "Based on your input, it sounds like you are looking at a domestic rabbit that's been abandoned and is at risk. Please try reaching out to an animal rescue local to your area to see if they can determine and get it to safety."
 let wildStr = `It sounds like you're looking at a wild rabbit. Here are some that matched your input so that you can compare and determine!`
 let maybeEither = `It sounds like you're looking at a wild rabbit, but it's difficult to confirm based on the data provided. Here are some wild rabbits that match your input so that you can compare. If you think it's domestic, please try reaching out to an animal rescue local to your area to see if they can help determine and get it to safety.`
-getRabbits();
 
 function errorMessage()  {   //why does this only work up here and not down with other functions?  
-   if (Object.keys(inputObj).length < 7 ) { //need better way to get length of the collection
-      //console.log(collection, [...collection].length); //why isnt this pulling up the length of the array?
+   if (Object.keys(inputObj).length < 7 ) { //try e.target here to get length of collection
       console.log("Please make sure you complete all the fields."); 
    }
    else {
@@ -22,59 +19,43 @@ function eliminators(inputObj) {
       console.log(domesticStr);
    }
    else {
-      //console.log("maybe wild");
-      //call the comparison function
       getRabbits();
    }
 }
 function getRabbits() {
    fetch("http://localhost:3000/rabbits") 
    .then(response => response.json()) 
-   .then(rabbits => rabbits.map(rabbit => { //how do i map only through indexes 2-7??
-         console.log(rabbit)
-         for (let i = 2; i < rabbit.length; i++) {
-         // for (let key in rabbit) { 
-         // if (Array.isArray(rabbit[key])) {
-         //    //console.log('array')  ^^this works. I just learned that arrays come up as objects when using typeof if they are object values!
-         //    if (rabbit[key].includes(inputObj[key])) {
-         //       counter++;
-         //       console.log("one point")
-         //    }
-         // }
-         // else {
-         //    if (inputObj[key] === rabbit[key]) {
-         //       counter++;
-         //       console.log("one point")
-         //    }
-         // }      
-      // if (counter === 5) { //if I figure out how to get the collection length, change the 7 to collection.length
-      //    rabbitMatches.push(rabbit);
-         }
-      }))
-      // }
-   // }
+   .then(rabbits => rabbits.filter(rabbit => { 
+   returnMatches(rabbit);
+   renderRabbit(); 
+}))
+}
+
+function returnMatches(rabbit) {
+   let match = false; 
+   // rabbit.map(key => {    
+   //    inputObj.map(key => { 
+   //    })
    // })
-   // let counter = 0; 
-
-   //    for (let key in rabbit) {      
-   //       if (Array.isArray(rabbit[key])) {
-   //          //console.log('array')  ^^this works. I just learned that arrays come up as objects when using typeof if they are object values!
-   //          if (rabbit[key].includes(inputObj[key])) {
-   //             counter++;
-   //          }
-   //       }
-   //       else {
-   //          if (inputObj[key] === rabbit[key]) {
-   //             counter++;
-   //          }
-   //       }
-   //    }      
-   //    if (counter === 5) { //if I figure out how to get the collection length, change the 7 to collection.length
-   //       rabbitMatches.push(rabbit);
-     // }  
-   //))}
-   }
-
+      for (let key in rabbit) { 
+         for (let key in inputObj) {    
+            if (rabbit[key] === "id" || key === "image" || key === "species") {
+               match = true;
+            }
+            else if (Array.isArray(rabbit[key]) && rabbit[key].includes(inputObj[key])) { 
+               match = true; 
+            }
+            else if (rabbit[key] === inputObj[key]) {
+               match = true;
+            }   
+         }   
+      }
+      if (match === true) {
+         matchArr.push(rabbit.species)
+      }
+      else {
+      }
+}
 document.addEventListener('DOMContentLoaded', () => {
    getInput();
    //  let region = document.getElementsByTagName("select")[0];
@@ -92,6 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
        errorMessage(); 
    })
 })
+
 function getInput() {
    let categories = document.getElementsByTagName("select");
    for (let i = 0; i < categories.length; i++) {
@@ -102,15 +84,19 @@ function getInput() {
      })
      
    }
-
-// function compareToData() {
-//    getRabbits();
-//    //narrow by region first? inputObj.region => return rabbits with same region. then eyes, then ears, then coat, then size, then color, then body shape
-// }
+}
+      
+function renderRabbit() {
+   if (matchArr.length === 1) {
+      console.log(`We've got a match! It sounds like a ${matchArr}.`)
+   }
+   else if (matchArr.length === 2) {
+      console.log("We found multiple match possibilities: " + matchArr[0] + " and a " + matchArr[1] +"."); 
+   }
+}
 //return fetch request on the matches. append wiki frame and image into html? 
 
-
-         //   if category.value === " ") { why didn't this work?
+         //   if category.value === " ") { try this using e.target
          //       console.log("Please make sure you complete all the fields.")
          //   }
          //   else {
@@ -120,4 +106,3 @@ function getInput() {
 //event listener on select form submission. if option.value === holder, they didn't answer everything, error message. else, execute rabbitInput function
 //rabbitInput fn saves option.value to a variable named ${category} and push it into newObj
 //compare newObj to stored ones -- call comparisonFn
-}
