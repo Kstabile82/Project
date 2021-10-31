3c62463dbfc650635adbebac6121dfeced731a73
KarinaStabile <karina.stabile@gmail.com>
2021-10-31 01:22:47 -0400

    Updated reset function and added radio buttons and showWildRabbits function

--- Rabbits-Phase-1-Project/Rabbits.js
+++ Rabbits-Phase-1-Project/Rabbits.js
@@ -3,25 +3,13 @@ let inputObj = {
 let matchArr = []; 
 let button = document.createElement('btn');
 let collection = document.getElementsByTagName('select');
-let domesticStr = "Based on your input, it sounds like you are looking at a domestic rabbit that's been abandoned and needs help. Please try reaching out to an animal rescue local to your area to see if they can determine and get it to safety."
 
 document.addEventListener('DOMContentLoaded', () => {
    getInput();
    document.getElementById('rabbitform').addEventListener('submit', (e) => {
        e.preventDefault(); 
        errorMessage(); 
-   })
-   button.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.getElementById("rabbitform").reset();
-      document.getElementById("rabbitform").style.display="block" 
-      let rabbitmatches = document.getElementById("rabbit matches");
-      while (rabbitmatches.firstChild) {
-         rabbitmatches.removeChild(rabbitmatches.firstChild);
-      }
-      //button.innerText = " "
-      button.remove();
-   })
+   })    
 })
 
 function errorMessage()  {    
@@ -37,7 +25,7 @@ function errorMessage()  {
 function eliminators(inputObj) {
    if (inputObj["ears"] === "Floppy" || inputObj["color"] === "Other" || inputObj["eyes"] === "Red/pink" || inputObj["eyes"] === "Blue" || inputObj["coat"] === "Spotted"){
       let p = document.createElement("p");
-      p.innerText = domesticStr;
+      p.innerText = "Based on your input, it sounds like you are looking at a domestic rabbit that's been abandoned and needs help. Please try reaching out to an animal rescue local to your area to see if they can determine and get it to safety."
       document.getElementById("rabbit matches").appendChild(p);
       resetForm(); 
    }
@@ -52,6 +40,14 @@ function getRabbits() {
    .then(rabbits => {
       returnMatches(rabbits)
    })
+}
+
+function categoryTest(category, inputObj, rabbits) {
+     let matches = rabbits.filter(function(rabbit) {
+      return rabbit[category] === inputObj[category] || rabbit[category].includes(inputObj[category])
+   })
+      // can i do this instead of returnMatches below, and use recursion? Base case would be: matches = rabbits.length? or 0? 
+   return matches; 
 }
 
 function returnMatches(rabbits) {
@@ -76,7 +72,7 @@ function returnMatches(rabbits) {
    sizeMatch.filter(match => {
      matchArr.push(match["species"])
    })
-   renderRabbit(matchArr)
+   renderRabbit(matchArr, rabbits)
 }   
 
 function getInput() {
@@ -89,8 +85,7 @@ function getInput() {
    }
 } 
 
-function renderRabbit(matchArr) { 
-   inputObj = {}; 
+function renderRabbit(matchArr, rabbits) { 
    let matchString = "We found multiple match possibilities: "; 
    let p = document.createElement("p");
    document.getElementById("rabbit matches").appendChild(p);
@@ -113,9 +108,8 @@ function renderRabbit(matchArr) {
    }
    else if (matchArr.length === 0) {
       p.innerText = "No matches came up in our search, so it sounds like you are looking at a domestic rabbit that needs help, and suggest that you reach out to a local animal rescue group. But just in case, do you want to see a listing of all wild rabbit species that are found in your area so that you can compare?"
-      //showWildButton(); 
+      showWildButton(inputObj, rabbits); 
    }
-
    resetForm();
 }
 
@@ -149,25 +143,69 @@ function searchNature(idx) {
 }
 
 function resetForm() {
-   //move all button stuff to here including global var declaration
    let rabbitfinder = document.getElementById("rabbit finder");
    rabbitfinder.appendChild(button)
    button.innerText = "Reset Form"
+   button.addEventListener("click", (e) => {
+      e.preventDefault();
+      homePage(); 
+   })
+} 
+
+function homePage() {
    inputObj = {}; 
    matchArr = []; 
+   document.getElementById("rabbitform").reset();
+      document.getElementById("rabbitform").style.display="block" 
+      let rabbitmatches = document.getElementById("rabbit matches");
+      while (rabbitmatches.firstChild) {
+         rabbitmatches.removeChild(rabbitmatches.firstChild);
+      }
+      button.remove();
 }
 
-function showWildButton() {
-   //make this a cable box
-   let wildButton = document.createElement("btn"); 
-   wildButton.innerText = "Yes"; 
-   let noButton = document.document.createElement("btn"); 
-   noButton.innerText = "No"; 
-   document.getElementById("rabbit matches").appendChild(wildButton);
-   document.getElementById("rabbit matches").appendChild(noButton);
-   document.addEventListener(wildButton)
-   //if noButton, resetForm(); 
-   //if yesButton, push all region matches into matchArr and call searchNature 
+function showWildButton(inputObj, rabbits) {
+   let radioForm = document.createElement("form");
+   document.getElementById("rabbit matches").appendChild(radioForm);
+   let yesButton = document.createElement("input"); 
+   yesButton.type = "radio";
+   yesButton.name = "wildBunnies"
+   yesButton.value = "yes";
+   radioForm.appendChild(yesButton); 
+   let yesLabel = document.createElement('label');
+   yesLabel.textContent = "Yes"
+   radioForm.appendChild(yesLabel)
+   let noButton = document.createElement("input"); 
+   noButton.type = "radio";
+   noButton.name = "wildBunnies";
+   noButton.value = "no";
+   radioForm.appendChild(noButton);
+   let noLabel = document.createElement('label');
+   noLabel.textContent = "No";
+   radioForm.appendChild(noLabel);
+   radioForm.addEventListener('change', (e) => {
+      e.preventDefault();
+      if (e.target.value === 'yes') {
+         showWildRabbits(inputObj, rabbits); 
+      }
+      else {
+         homePage();
+      }
+   })
+}
+
+function showWildRabbits(inputObj, rabbits) {
+//    let regionMatch = rabbits.filter(function (rabbit) {
+//       return rabbit.region === inputObj.region || rabbit.region.includes(inputObj.region)  
+//    })
+   let regionMatch = categoryTest("region", inputObj, rabbits); 
+   console.log(regionMatch); 
+     regionMatch.filter(match => {
+         matchArr.push(match["species"])
+       })  
+   matchArr.map(idx => {
+      searchNature(idx);
+})
 }
 
    //   let div = document.createElement('div')
@@ -179,81 +217,9 @@ function showWildButton() {
    // document.getElementById("rabbit finder").appendChild(iframe);
    //div.innerHTML = data.results[0].preferred_common_name; 
 
-// function searchFlickr (rabbit) {
-//    if (rabbit.includes(" ")) {
-//      rabbit.replaceAll(" ", " + "); 
-//      }
-//     fetch (`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=4b131cfca539bd6b4ea5252f067c1bbe&tags=${rabbit}&format=json&nojsoncallback=1&`)
-//       .then(function(response) {
-//          return response.json();
-//       })
-//       .then(data => {
-//          for (let key in data) {
-//             let secondKey = data[key]; 
-//             for (let key in secondKey) {
-//                let thirdKey = secondKey[key];
-//                for (let key in thirdKey) {
-//                   let fourthKey = thirdKey[key];
-//                   for (let key in fourthKey) {
-//                      if (key === "title") {
-//                         if(fourthKey[key].toLowerCase().includes(rabbit.toLowerCase())) {
-//                            console.log(fourthKey[key])
-//                         }
-//                      }
-//                   }
-//                }
-//             }
-//          }
-
-//       })
-//  }
-
-
-//      for (let key in data) {
-//            let secondKey = data[key];
-//               for (let key in secondKey) {
-//                 if (key === "search") {
-//                    let search = secondKey[key];
-//                    //array of objects containing more objects
-//                     search.map(item => {
-//                       for (let key in item) {
-//                          if (key === "title") {
-//                             let title = item[key]; 
-//                             if (title.toLowerCase() === rabbit.toLowerCase()) {
-//                                console.log(item)
-//                               }
-//                            }
-//                         }
-//                      })
-//                   }
-//                }  
-//        }
-//    })
-// }
-
-
-
-//flickr.galleries.getPhotos api_key=d9416fc65dae6717c665c2db49b1580e&photoset_id=&user_id=&page=1&per_page=4&
-
-//https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
-//https://live.staticflickr.com/7372/12502775644_acfd415fa7_w.jpg
-
-//api key="d9416fc65dae6717c665c2db49b1580e" photoset id="" user id="" page="1" per page = "4"
-//Flickr
-//flickr.activity.userPhotos
-//flickr.photos.search
-//flickr.photos.licenses.getInfo
-//flickr.photosets.getPhotos
-//<photoset id="4" primary="2483" page="1" perpage="500" pages="1" total="2">
-// {/* <photo id="2484" secret="123456" server="1" title="my photo" isprimary="0" />
-// <photo id="2483" secret="123456" server="1" title="flickr rocks" isprimary="1" />
-// </photoset> *//}
-
-//Key:
-//d9416fc65dae6717c665c2db49b1580e
-
-// Secret:
-// 472cc1aaf8a2a2bb
+//FLICKR
+//Key: d9416fc65dae6717c665c2db49b1580e
+//Secret: 472cc1aaf8a2a2bb
 
 // function filterFunc(rabbits) {
 // //could also convert each rabbit to array using Object.entries(rabbit).slice() and slice off first 2 and last idx, and convert inputObj, and compare...loop through array, if idx 1 isArray, use the conditionals below
@@ -278,19 +244,6 @@ function showWildButton() {
 //       }
 //    })
 // }
-
-   // for (let key in inputObj) {
-   //    rabbits.filter(function(rabbit) {
-        // console.log(rabbit[rabbitKey] === inputObj[key] || rabbit[rabbitKey].includes(inputObj[key]))
-      // })
-   
-
-         //   if category.value === " ") { try this using e.target
-         //       console.log("Please make sure you complete all the fields.")
-         //   }
-         //   else {
-         //       console.log(inputObj)
-         //   }
 
 //https://en.wikipedia.org/w/api.php?action=query&prop=value&. .& format=json
 //. . srsearch=Craig%20Noone indicates the page title or content to search for. The %20 indicates a space character in a URL.
